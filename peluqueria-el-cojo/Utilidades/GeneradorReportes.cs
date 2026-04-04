0d70168beb77e1c8113df298f8f65a6b2d2ce531
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2026-04-03 22:15:37 -0400

    1.1 Estructura de carpetas con clases base

---
+++ peluqueria-el-cojo/Utilidades/GeneradorReportes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peluqueria_el_cojo.Utilidades
+{
+    internal class GeneradorReportes
+    {
+    }
+}
